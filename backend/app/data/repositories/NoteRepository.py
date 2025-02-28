afac85f6b9bbadc0de672dc556562d6944662f74
shadowik <zahar.mark@bk.ru>
2025-02-28 11:42:04 +0400

    Add all notes

--- backend/app/data/repositories/NoteRepository.py
+++ backend/app/data/repositories/NoteRepository.py
@@ -10,8 +10,12 @@ notes_data = [
     {"id": 4, "name": "D#"},
     {"id": 5, "name": "E"},
     {"id": 6, "name": "F"},
-    {"id": 7, "name": "G"},
-    {"id": 8, "name": "D#"}
+    {"id": 7, "name": "F#"},
+    {"id": 8, "name": "G"},
+    {"id": 9, "name": "G#"},
+    {"id": 10, "name": "A"},
+    {"id": 11, "name": "A#"},
+    {"id": 12, "name": "B"}
 ]
 
 
