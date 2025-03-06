7ff2f7b4e34718989a54c1de1ff2d1e342a26de0
shadowik <zahar.mark@bk.ru>
2025-03-06 16:01:44 +0400

    Initial conf

---
+++ backend/app/utils/AudioLDM2Generator.py
@@ -0,0 +1,28 @@
+from diffusers import AudioLDM2Pipeline
+import scipy
+import torch
+
+
+class AudioLDM2Generator:
+    def __init__(self, model_name="cvssp/audioldm2"):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.pipeline = AudioLDM2Pipeline.from_pretrained(model_name)
+        self.pipeline.to(self.device)
+
+    def generate_audio(self, prompt, num_samples=1, output_path="output.wav"):
+        for i in range(num_samples):
+            result = self.pipeline(
+                prompt=prompt,
+                audio_length_in_s=3.0,
+                num_inference_steps=100
+            ).audios[0]
+
+            rate = 16000
+            output_file = f"{output_path.split('.')[0]}_{i}.wav"
+
+            scipy.io.wavfile.write(output_file, rate, data=result)
+
+
+if __name__ == "__main__":
+    ldm_model = AudioLDM2Generator()
+    ldm_model.generate_audio("Meow sound")
