9ebb8330b1cabf9f994f890ccc354661128f7b09
shadowik <zahar.mark@bk.ru>
2025-04-10 23:45:04 +0400

    Fix

--- backend/app/utils/AudioLDM2Generator.py
+++ backend/app/utils/AudioLDM2Generator.py
@@ -23,7 +23,7 @@ class AudioLDM2Generator:
         samples_name = []
         for i in range(sample_req.count):
             result = cls.pipeline(
-                prompt=sample_req.count,
+                prompt=sample_req.text_request,
                 negative_prompt=sample_req.negative_prompt,
                 audio_length_in_s=sample_req.audio_length_in_s,
                 num_inference_steps=sample_req.num_inference_steps,
